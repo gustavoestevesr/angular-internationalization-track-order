5203bd40596a42494f5d50add665b7b063b951aa
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-02-01 20:46:27 -0300

    feat: definição de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { OrdersListComponent } from './pages/orders-list/orders-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: OrdersListComponent,
+  },
+  {
+    path: '**',
+    component: OrdersListComponent,
+  }
+];
