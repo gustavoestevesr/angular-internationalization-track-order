d08ef9d8403a672cea78815d284da780e2ebfd0e
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-02-01 20:48:14 -0300

    refactor: remoção template e tradução título da página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Title } from '@angular/platform-browser';
+import {
+  Item,
+  StatusOrderModalComponent,
+} from './dialog/status-order-modal/status-order-modal.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDialogModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-internationalization-track-order';
+  title = 'angular internationalization track order';
+
+  readonly titleService = inject(Title);
+
+  constructor() {
+    this.titleService.setTitle($localize`${this.title}`);
+  }
 }
