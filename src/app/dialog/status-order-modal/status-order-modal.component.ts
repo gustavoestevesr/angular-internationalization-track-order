0187fc408e4d9342e9f2c78479ea2f6841e205e2
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-02-01 20:46:08 -0300

    feat: status order modal implementado para visualizar compras

--- src/app/dialog/status-order-modal/status-order-modal.component.ts
+++ src/app/dialog/status-order-modal/status-order-modal.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+
+export interface Item {
+  name: string;
+  quantity: number;
+  date: Date;
+  price: number;
+  status: 'confirmed' | 'in production' | 'in route' | 'delivered';
+}
 
 @Component({
   selector: 'app-status-order-modal',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, DatePipe, CurrencyPipe],
   templateUrl: './status-order-modal.component.html',
-  styleUrl: './status-order-modal.component.scss'
+  styleUrl: './status-order-modal.component.scss',
 })
 export class StatusOrderModalComponent {
+  totalAmount = 0;
 
+  readonly data: { items: Item[] } = inject(MAT_DIALOG_DATA);
+
+  constructor() {
+    this.data.items.forEach((item) => this.totalAmount = item.quantity * item.price);
+  }
 }
